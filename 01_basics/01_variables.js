9c7498a435778655fc43c8d2fd7330ea40894c18
ameykulkarni10 <39147555+ameykulkarni10@users.noreply.github.com>
2023-07-26 17:19:28 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "amey@google.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+
+let accountState;
+
+//accountId = 2 //not allowed
+
+accountEmail = "ak@ak.com"
+accountPassword = "21212121"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+
+console.log(3+3);
+
+
+console.log("Amey")
+
+
+
+let name = "Amey"
+let age = 28
+let isLoggedIn = true
+let state = null
+
+
+
+// Primitive Date Types
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => this is standalone value
+// undefined => 
+// symbol => unique
+
+
+//object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
